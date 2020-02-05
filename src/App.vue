0d41316d9c24818670ebae1e86de8749fe88d9bc
Bilal Haidar <bhaidar@gmail.com>
2020-02-06 01:31:52 +0200

    Add global scss support

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,14 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: $color;
 }
 
 #nav {
